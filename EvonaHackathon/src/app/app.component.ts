3d4e9cb219bd38778d425baa0071a9a500180327
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2021-10-15 23:13:03 +0200

    First version of the card

--- EvonaHackathon/src/app/app.component.ts
+++ EvonaHackathon/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { MyComponentComponent } from './my-component/my-component.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'EvonaHackathon';
+  title = 'Evona u srcu <3';
+
+
+  ngOnInit() {
+  }
 }
---
+++ EvonaHackathon/src/app/my-component/my-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
