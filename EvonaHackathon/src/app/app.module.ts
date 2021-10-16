5da6958530deab35c31b67825f20091aa1dcb766
Admir Ljevo <admir.ljevo@edu.fit.ba>
2021-10-16 18:50:31 +0200

    Header i carousel bannera

--- EvonaHackathon/src/app/app.module.ts
+++ EvonaHackathon/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BanneriComponent } from './components/banneri/banneri.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BannerSliderComponent } from './components/banner-slider/banner-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BanneriComponent,
+    BannerSliderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
