4de6922dcda191a3077e9c83d9cf49506c0fa742
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2021-10-17 00:16:24 +0200

    added servis

---
+++ EvonaHackathon/src/app/servisi/matches.servis.ts
@@ -0,0 +1,32 @@
+import { Injectable} from "@angular/core";
+import { HttpClient, HttpClientModule, HttpHeaders } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MatchesServis{
+
+    post={};
+    readonly ROOT_URL = 'https://stageapis.bet-x.eu/SportOfferApi/api/sport/offer/v3/matches/live'
+
+    posts:any;
+    sports: any[] = [];
+
+    constructor(private http:HttpClient){
+      this.posts = this.http.get(this.ROOT_URL).subscribe(s=>{
+        this.sports = s as any;
+        //console.log("Ovo je post",this.sports);
+      });
+    }
+    getMatches(){
+        return this.http.get(this.ROOT_URL, )
+    }
+    toogle:boolean=true;
+    ngOnInit(): void {}
+
+
+
+
+
+}
