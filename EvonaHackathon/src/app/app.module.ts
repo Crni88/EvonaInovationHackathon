349028cdc44eb2ae9057f490993497d893598020
Amir Karaga <83365970+amir-karaga@users.noreply.github.com>
2021-10-16 18:41:16 +0200

    Added footer, menu and page component

--- EvonaHackathon/src/app/app.module.ts
+++ EvonaHackathon/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { IzbornikComponent } from './izbornik/izbornik.component';
+import { PageComponent } from './page/page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    IzbornikComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
