a44dfcc06bd19f740e737c3cb34016b8ac3a76ed
Azur Delibašić <66976289+AzurDelibasic@users.noreply.github.com>
2021-10-17 11:28:27 +0200

    Finished registration form

--- EvonaHackathon/src/app/app.module.ts
+++ EvonaHackathon/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationSuccessComponent } from './registration-success/registration-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    RegistrationSuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- EvonaHackathon/src/app/registration/registration.component.ts
+++ EvonaHackathon/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,ReactiveFormsModule, FormGroup, Validators, EmailValidator} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  formaRegistracija! : FormGroup;
   constructor() { }
-
   ngOnInit(): void {
-    
+    this.formaRegistracija = new FormGroup({
+      username : new FormControl('',[Validators.required,Validators.minLength(4)]),
+      password: new FormControl('',[Validators.required,Validators.minLength(8)]),
+      confirmpassword : new FormControl('',[Validators.required,Validators.minLength(8)]),
+      email : new FormControl('',[Validators.required,Validators.email]),
+
+    })
+
+
   }
 
 }
