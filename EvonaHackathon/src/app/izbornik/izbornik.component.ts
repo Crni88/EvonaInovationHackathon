05f5475d4a35720ec5fcdc828e5b55266df04382
Amir Karaga <83365970+amir-karaga@users.noreply.github.com>
2021-10-17 12:17:15 +0200

    bug fixes

--- EvonaHackathon/src/app/izbornik/izbornik.component.ts
+++ EvonaHackathon/src/app/izbornik/izbornik.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IzbornikComponent implements OnInit {
 
- 
+  selectedSportId:any;
   constructor() {
     
    }
@@ -19,37 +19,5 @@ export class IzbornikComponent implements OnInit {
   toogle1:boolean=true;
   toogle2:boolean=false;
   toogle3:boolean=false;
-  enableDisableRule(id:string):void{
-    console.log("click");
-    if(id==="obruc1")
-    {
-      if(!this.toogle1)
-      {
-        this.toogle1=!this.toogle1;
-      this.toogle2=false;
-      this.toogle3=false;
-      }
-      
-    }
-    else if(id==="obruc2")
-    {
-      if(!this.toogle2)
-      {
-        this.toogle2=!this.toogle2;
-      this.toogle1=false;
-      this.toogle3=false;
-      }
-    }
-    else if(id==="obruc3")
-    {
-      if(!this.toogle3)
-      {
-      this.toogle3=!this.toogle3;
-      this.toogle1=false;
-      this.toogle2=false;
-      }
-    }
-   
-  }
-
+ 
 }
